2180e744dac9885c151b69a5a9d552e94aaafef8
Mauro Mendes Nogueira <mauro@mmsites.com.br>
2020-09-13 21:53:03 -0300

    Alteração realizada em 13/09/2020 às 21:40 Hs

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
-        <title>Titulo aqui</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> 
+	<title>Titulo aqui</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> 
     </head>
 <body>
+	
+	
     <div class="container">
 
 	<center>
-<h1>Arquivo inicial para o github!</h1>
-<h4>Curso PHP  Hcode e Udemy</h4>
-<p>Alteração realizada em 13/09/2020 às 21:30Hs</p>
-<p>Alteração realizada em 13/09/2020 às 21:36 Hs</p>
-<p>Alteração realizada em 13/09/2020 às 21:40 Hs</p>
+	<h1>Arquivo inicial para o github!</h1>
+	<h4>Curso PHP  Hcode e Udemy</h4>
 
-
-
-
-
-
-
+	<p>Alteração realizada em 13/09/2020 às 21:30Hs</p>
+	<p>Alteração realizada em 13/09/2020 às 21:36 Hs</p>
+	<p>Alteração realizada em 13/09/2020 às 21:40 Hs</p>		
         
 	</center>
 
     </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	
+	
 
 </body>
 </html>
